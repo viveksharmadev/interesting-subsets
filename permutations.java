c824853764406c72777d9348d14b7b68600dfdcf
Learner <vivek.gurukul1982@gmail.com>
2020-09-19 22:28:50 -0700

    Update permutations.java

--- permutations.java
+++ permutations.java
@@ -1,23 +1,47 @@
 // https://leetcode.com/problems/permutations/
 class Solution {
-    // tc -> n*n!, sc-> n*n!
+    // tc-> n*n!, sc-> n*n!
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new LinkedList<>();
-        calculatePermutations(nums, new LinkedList<>(), res);
-        return res;
+        // Result
+        List<List<Integer>> result = new LinkedList<>();
+        
+        // Base condition
+        if(nums==null || nums.length==0)
+            return result;
+        
+        // Permutations calculation
+        permutations(nums, new LinkedList<>(), result, new boolean[nums.length]);
+        
+        // Return Result
+        return result;
     }
     
-    private void calculatePermutations(int[] nums, 
-                                 List<Integer> list, List<List<Integer>> res){
-             
+    private void permutations(int[] nums, List<Integer> list, 
+                             List<List<Integer>> result,
+                             boolean[] visited){
+        // Base Condition
         if(list.size()==nums.length){
-            res.add(new LinkedList(list));
+            result.add(new LinkedList(list));
         }
-        for(int i=0; i<nums.length; i++){           
-            if(list.contains(nums[i])) continue;
+        
+        // Traverse the given array and populate the intermediate list
+        for(int i=0; i<nums.length; i++){
+            // Check if number is already visited, then skip it
+            if(visited[i]) continue;
+            
+            // Add to intermediate list and mark it as visited
             list.add(nums[i]);
-            calculatePermutations(nums, list, res);
+            visited[i] = true;
+            
+            // Recursion to find all permutations
+            permutations(nums, list, result, visited);
+            
+            // Backtrack
             list.remove(list.size()-1);
+            visited[i] = false;
         }
+        
     }
+    
+    
 }
